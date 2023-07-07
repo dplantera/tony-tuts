1e875db50279e8df9d2b7d8edbb4b238e129dbed
dplantera <d-plantera@outlook.com>
2023-07-07 12:55:36 +0300

    lessen 00

---
+++ snake-beginner/lesson-00/spiel.js
@@ -0,0 +1,70 @@
+/* lesson-01
+ * - wir holen uns HtmlElemente und speichern sie in globalen variablen:
+ *       canvasHtmlElement, zeichenKontext
+ * - wir erstellen uns HtmlElemente und speichern sie in globalen variablen:
+ *       scoreElement, hudElement
+ * - wir erstellen und verwenden Hilfs-Funktionen
+ *       "zeichneRechteck", welche uns das zeichnen etwas erleichtert
+ *       "gibZufälligePosition", welche uns eine zufällige Position auf dem Spielfeld ausgibt
+ * - wir zeichnen:
+ *       ein Rechteck
+ *       das Spielfeld
+ *       die Spielfigur "snake", erstmals mit einer festen position
+ *       ein Pick-Up Item "food", erstmals mit einer festen position
+ *       den initialen Punktestand "score" in einem entsprechenden HtmlElement
+ * - wir updaten:
+ *       die Bewegungsrichtung von "snake", in dem wir eine globale Variable "playerDirection" je nach tastendruck von W, S, A, D neu setzen auf 'HOCH' | 'RUNTER' | 'LINKS' | 'RECHTS'  
+ *       die Bewegung von "snake", in dem wir die x- und y-Koordinate von "snake" jeweils in einer globalen Variable speichern und mit Hilfe der Bewegungsrichtung neu setzen 
+ *       den Punktestand "score" je nach "Spielregel", indem wir die globale Variable "score" neu setzen
+ *       die Position von "food" je nach "Spielregel", indem wir die x- und y-Koordinate von "food" jeweils in einer globalen Variable speichern und mit Hilfe der Funktion "gibZufälligePosition" neu setzen
+ * - wir implementieren Spielregeln:
+ *       "snake" darf nie das Spielfeld verlassen - wenn "snake" die Spielfeldgrenzen erreicht, dann bleibt die Figur vor der Grenze einfach stehen. 
+ *       wenn wir die Leertaste drücken, wollen wir das Spiel pausieren.
+ *       wenn "snake" das item "food" erreicht bzw "isst", dann:
+ *              zählen wir den Punktestand um eins hoch (Inkrementieren)  
+ *              verschieben wir "food" auf eine neue zufällige Position
+ */
+
+
+/**
+ * ######################################
+ * # Animation
+ */
+export function update(){
+}
+
+export function zeichne(time) {
+}
+
+export function beiStart(){
+}
+
+
+/**
+ * ######################################
+ * # Html Elemente / html elements
+ */
+
+
+/**
+ * ######################################
+ * # spielbrett / game board
+ */
+
+
+/**
+ * ######################################
+ * # snake (player)
+ */
+
+
+
+/** ######################################
+ *  # essen / food (pick-up item)
+ */
+
+/**
+ * ######################################
+ * # Hilfsfunktionen / helper functions
+ */
+
