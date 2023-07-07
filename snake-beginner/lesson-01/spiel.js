b08e3f7f3d30f812ee3ee64e5eb4e4c69e0b1cab
dplantera <d-plantera@outlook.com>
2023-07-07 12:44:28 +0300

    added description

--- snake-beginner/lesson-01/spiel.js
+++ snake-beginner/lesson-01/spiel.js
@@ -1,3 +1,35 @@
+/* lesson-01
+ * - wir holen uns HtmlElemente und speichern sie in globalen variablen:
+ *       canvasHtmlElement, zeichenKontext
+ * - wir erstellen uns HtmlElemente und speichern sie in globalen variablen:
+ *       scoreElement, hudElement
+ * - wir erstellen und verwenden Hilfs-Funktionen
+ *       "zeichneRechteck", welche uns das zeichnen etwas erleichtert
+ *       "gibZufälligePosition", welche uns eine zufällige Position auf dem Spielfeld ausgibt
+ * - wir zeichnen:
+ *       ein Rechteck
+ *       das Spielfeld
+ *       die Spielfigur "snake", erstmals mit einer festen position
+ *       ein Pick-Up Item "food", erstmals mit einer festen position
+ *       den initialen Punktestand "score" in einem entsprechenden HtmlElement
+ * - wir updaten:
+ *       die Bewegungsrichtung von "snake", in dem wir eine globale Variable "playerDirection" je nach tastendruck von W, S, A, D neu setzen auf 'HOCH' | 'RUNTER' | 'LINKS' | 'RECHTS'  
+ *       die Bewegung von "snake", in dem wir die x- und y-Koordinate von "snake" jeweils in einer globalen Variable speichern und mit Hilfe der Bewegungsrichtung neu setzen 
+ *       den Punktestand "score" je nach "Spielregel", indem wir die globale Variable "score" neu setzen
+ *       die Position von "food" je nach "Spielregel", indem wir die x- und y-Koordinate von "food" jeweils in einer globalen Variable speichern und mit Hilfe der Funktion "gibZufälligePosition" neu setzen
+ * - wir implementieren Spielregeln:
+ *       "snake" darf nie das Spielfeld verlassen - wenn "snake" die Spielfeldgrenzen erreicht, dann bleibt die Figur vor der Grenze einfach stehen. 
+ *       wenn wir die Leertaste drücken, wollen wir das Spiel pausieren.
+ *       wenn "snake" das item "food" erreicht bzw "isst", dann:
+ *              zählen wir den Punktestand um eins hoch (Inkrementieren)  
+ *              verschieben wir "food" auf eine neue zufällige Position
+ */
+
+/**
+ * ######################################
+ * # Html Elemente
+ */
+
 /***  @type {HTMLCanvasElement} */
 const canvasHtmlElement = document.getElementById("spiel-brett");
 /***  @type {CanvasRenderingContext2D} */
@@ -33,7 +65,7 @@ const snakeSize = 10;
 let snakeX;
 let snakeY;
 /** @type {'HOCH' | 'RUNTER' | 'LINKS' | 'RECHTS'} */
-let richtung = 'RECHTS';
+let playerDirection = 'RECHTS';
 function setzeSnake(){
     const pos = gibZufälligePosition(snakeSize);
     snakeX = pos.x;
@@ -41,16 +73,16 @@ function setzeSnake(){
 }
 
 function bewegeSnake() {
-    if(richtung === 'LINKS'){
+    if(playerDirection === 'LINKS'){
         snakeX = snakeX - snakeSize;
      }
-    if(richtung === 'RECHTS'){ 
+    if(playerDirection === 'RECHTS'){ 
         snakeX = snakeX + snakeSize;
     }
-    if(richtung === 'HOCH'){ 
+    if(playerDirection === 'HOCH'){ 
         snakeY = snakeY - snakeSize;
     }
-    if(richtung === 'RUNTER'){ 
+    if(playerDirection === 'RUNTER'){ 
         snakeY = snakeY + snakeSize;
     }
 
@@ -130,21 +162,21 @@ export function beiStart(){
  */
 function beiTastatur(event){
     let key = event.key;
-    if(key === 'a' && richtung !== 'RECHTS'){
-        richtung = 'LINKS'
+    if(key === 'a' && playerDirection !== 'RECHTS'){
+        playerDirection = 'LINKS'
      }
-    if(key === 'd' && richtung !== 'LINKS'){ 
-        richtung = 'RECHTS'
+    if(key === 'd' && playerDirection !== 'LINKS'){ 
+        playerDirection = 'RECHTS'
     }
-    if(key === 'w' && richtung !== 'RUNTER'){ 
-        richtung = 'HOCH'
+    if(key === 'w' && playerDirection !== 'RUNTER'){ 
+        playerDirection = 'HOCH'
     }
-    if(key === 's' && richtung !== 'HOCH'){ 
-        richtung = 'RUNTER'
+    if(key === 's' && playerDirection !== 'HOCH'){ 
+        playerDirection = 'RUNTER'
     }
     if(key === ' '){ 
         console.log("pause")
-        richtung = ''
+        playerDirection = ''
     }
 }
 
